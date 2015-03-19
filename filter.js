f5064939f3d6f34388a4fa507fd598296ac54245
Ethan Harstad <ethanharstad@gmail.com>
2015-03-19 11:13:46 -0500

    Change filter api to factory pattern

--- filter.js
+++ filter.js
@@ -1,63 +1,54 @@
 // http://www.aprs-is.net/javAPRSFilter.aspx
 
-var Filter = function() {
-  this.filterString = '';
-}
+var Filter = {
+  rangeFilter: function(point, range) {
+    return 'r/' + point[1] + '/' + point[0] + '/' + range;
+  },
 
-Filter.prototype.rangeFilter = function(point, range) {
-  if(this.filterString.length > 0) {
-    this.filterString += ' ';
-  }
-  this.filterString += 'r/' + point[1] + '/' + point[0] + '/' + range
-  return this;
-}
-
-Filter.prototype.prefixFilter = function(prefixes) {
-  if(this.filterString.length > 0) {
-    this.filterString += ' ';
-  }
-  this.filterString += 'p';
-  if(prefixes.constructor === Array) {
-    var length = prefixes.length;
-    for(var i = 0; i < length; i++) {
-      this.filterString += '/' + prefixes[i];
+  prefixFilter: function(prefixes) {
+    var filter = 'p';
+    if(prefixes.constructor === Array) {
+      var length = prefixes.length;
+      for(var i = 0; i < length; i++) {
+        filter += '/' + prefixes[i];
+      }
+    } else {
+      filter += '/' + prefixes;
     }
-  } else {
-    this.filterString += '/' + prefixes;
+    return filter;
+  },
+
+  friendFilter: function(calls) {
+    return this;
+  },
+
+  objectFilter: function(objects) {
+    return this;
+  },
+
+  typeFilter: function(types, call, range) {
+    return this;
+  },
+
+  symbolFilter: function(primary, alternate, overlay) {
+    return this;
+  },
+
+  digipeaterFilter: function(digis) {
+    return this;
+  },
+
+  areaFilter: function(area) {
+    return this;
+  },
+
+  myRangeFilter: function(dist) {
+    return this;
+  },
+
+  friendRangeFilter: function(call, dist) {
+    return this;
   }
-  return this;
-}
-
-Filter.prototype.friendFilter = function(calls) {
-  return this;
-}
-
-Filter.prototype.objectFilter = function(objects) {
-  return this;
-}
-
-Filter.prototype.typeFilter = function(types, call, range) {
-  return this;
-}
-
-Filter.prototype.symbolFilter = function(primary, alternate, overlay) {
-  return this;
-}
-
-Filter.prototype.digipeaterFilter = function(digis) {
-  return this;
-}
-
-Filter.prototype.areaFilter = function(area) {
-  return this;
-}
-
-Filter.prototype.myRangeFilter = function(dist) {
-  return this;
-}
-
-Filter.prototype.friendRangeFilter = function(call, dist) {
-  return this;
 }
 
 module.exports = Filter;
---
+++ test/filter.js
@@ -0,0 +1,28 @@
+// Module under test
+var Filter = require('../filter');
+
+var assert = require('chai').assert;
+
+describe('Filter', function() {
+
+  describe('rangeFilter', function() {});
+
+  describe('prefixFilter', function() {});
+
+  describe('friendFilter', function() {});
+
+  describe('objectFilter', function() {});
+
+  describe('typeFilter', function() {});
+
+  describe('symbolFilter', function() {});
+
+  describe('digipeaterFilter', function() {});
+
+  describe('areaFilter', function() {});
+
+  describe('myRangeFilter', function() {});
+
+  describe('friendRangeFilter', function() {});
+
+});
